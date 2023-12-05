6a2fd3ae502edc00abbd12137885309da40ce0ac
Shahariar Kabir <maask5534@gmail.com>
2023-12-05 09:32:27 +0600

    set target

--- src/clients/brainlab/haletrailer/HT007/common/test.info.ts
+++ src/clients/brainlab/haletrailer/HT007/common/test.info.ts
@@ -1,8 +1,1 @@
-export enum TestInfo {
-  ID = "HT007",
-  SITE = "haletrailer",
-  TITLE = "4.1 service details Accordion vs. open list column",
-  CLIENT = "brainlab",
-  TARGET = "https://haletrailer.com/services/",
-  VARIATION = "1",
-}
+export enum TestInfo {ID = 'HT007', SITE = 'haletrailer', TITLE = '4.1 service details Accordion vs. open list column', CLIENT = 'brainlab', TARGET = 'https://haletrailer.com/services/', VARIATION = '1'}
