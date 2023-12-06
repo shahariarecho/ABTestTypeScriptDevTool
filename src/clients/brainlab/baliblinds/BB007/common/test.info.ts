76659eb56e8cc991558df179be31278a812ce95d
Shahariar Kabir <maask5534@gmail.com>
2023-12-06 18:44:13 +0600

    reduced description font weight

--- src/clients/brainlab/baliblinds/BB007/common/test.info.ts
+++ src/clients/brainlab/baliblinds/BB007/common/test.info.ts
@@ -1,8 +1,1 @@
-export enum TestInfo {
-  ID = "BB007",
-  SITE = "baliblinds",
-  TITLE = '4.9 - Lightbox CTA "View in visualizer" on swatch preview',
-  CLIENT = "brainlab",
-  TARGET = "https://www.baliblinds.com/swatches/",
-  VARIATION = "1",
-}
+export enum TestInfo {ID = 'BB007', SITE = 'baliblinds', TITLE = '4.9 - Lightbox CTA "View in visualizer" on swatch preview', CLIENT = 'brainlab', TARGET = 'https://www.baliblinds.com/swatches/', VARIATION = '1'}
