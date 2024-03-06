3c5d955f8737e6f48a29489fe11fde047b060b4a
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-03-06 12:49:54 +0600

    Fixed selector issue

--- src/clients/brainlab/baliblinds/BB006/common/asset.ts
+++ src/clients/brainlab/baliblinds/BB006/common/asset.ts
@@ -1,5 +1,5 @@
 export const selectors = {
-  glideSlidesNthList: "ul.glide__slides>li:nth-child",
+  glideSlidesNthList: "section.carousel-section ul.glide__slides>li:nth-child",
 };
 
 export const customEventName: string = "glide-3rd-or-6th-link-click";
--- src/clients/brainlab/baliblinds/BB006/common/test.info.ts
+++ src/clients/brainlab/baliblinds/BB006/common/test.info.ts
@@ -1,8 +1,1 @@
-export enum TestInfo {
-  ID = "BB006",
-  SITE = "baliblinds",
-  TITLE = "3.3 - Visualizer in 3rd position in Hero",
-  CLIENT = "brainlab",
-  TARGET = "https://www.baliblinds.com/",
-  VARIATION = "1",
-}
+export enum TestInfo {ID = 'BB006', SITE = 'baliblinds', TITLE = '3.3 - Visualizer in 3rd position in Hero', CLIENT = 'brainlab', TARGET = 'https://www.baliblinds.com/', VARIATION = '1'}
