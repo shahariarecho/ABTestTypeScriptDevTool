026bcc1184373782d81a3d0f73e4384541a0ac0e
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-03-18 21:58:41 +0600

    start working on SS004

---
+++ src/clients/brainlab/sunsetter/SS004/common/asset.ts
@@ -0,0 +1,62 @@
+export const selectors = {};
+
+export const quotationIcon: string = `
+<svg width="32" height="23" viewBox="0 0 32 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M31.7025 17.6843C31.7025 18.5202 31.5125 19.2612 31.1325 19.9072C30.7906 20.5151 30.3346 21.0281 29.7646 21.4461C29.1946 21.8641 28.5677 22.1871 27.8837 22.4151C27.2377 22.6051 26.6297 22.7001 26.0598 22.7001C25.1858 22.7001 24.2928 22.5101 23.3809 22.1301C22.5069 21.7121 21.709 21.1231 20.987 20.3632C20.265 19.6032 19.6761 18.6912 19.2201 17.6273C18.7641 16.5633 18.5361 15.3854 18.5361 14.0934C18.5361 11.8515 18.9541 9.93262 19.79 8.33669C20.664 6.74076 21.69 5.41082 22.8679 4.34687C24.0839 3.24492 25.3188 2.38996 26.5727 1.78198C27.8647 1.13601 28.9286 0.680032 29.7646 0.414044V2.23796C29.0046 2.69394 28.1877 3.18792 27.3137 3.7199C26.4777 4.25187 25.6988 4.87884 24.9768 5.60081C24.2928 6.32278 23.7229 7.13974 23.2669 8.0517C22.8109 8.96366 22.5829 9.98962 22.5829 11.1296C22.5829 11.6995 22.6399 12.1555 22.7539 12.4975C22.8679 12.8395 23.0009 13.1055 23.1529 13.2955C23.3429 13.4855 23.5139 13.6185 23.6659 13.6945C23.8559 13.7704 24.0079 13.8084 24.1219 13.8084C24.5398 13.8084 25.0528 13.6945 25.6608 13.4665C26.2688 13.2005 26.8387 13.0675 27.3707 13.0675C27.8647 13.0675 28.3587 13.1815 28.8526 13.4095C29.3846 13.5995 29.8596 13.9034 30.2776 14.3214C30.6956 14.7014 31.0375 15.1764 31.3035 15.7464C31.5695 16.3163 31.7025 16.9623 31.7025 17.6843ZM13.9264 17.5703C13.9264 18.4062 13.7364 19.1472 13.3565 19.7932C13.0145 20.4392 12.5585 20.9711 11.9885 21.3891C11.4185 21.7691 10.7916 22.0731 10.1076 22.3011C9.46163 22.4911 8.83466 22.5861 8.22669 22.5861C7.35273 22.5861 6.47876 22.3961 5.6048 22.0161C4.73084 21.5981 3.93288 21.0281 3.21091 20.3062C2.48894 19.5462 1.89997 18.6342 1.44399 17.5703C1.02601 16.5063 0.817017 15.3284 0.817017 14.0364C0.817017 11.7945 1.235 9.87562 2.07096 8.27969C2.94492 6.64577 3.97088 5.29683 5.14882 4.23287C6.36477 3.13092 7.59971 2.27596 8.85366 1.66799C10.1076 1.02202 11.1526 0.566037 11.9885 0.300049V2.18096C11.2286 2.63694 10.4116 3.13092 9.53763 3.6629C8.70166 4.19488 7.9227 4.82185 7.20073 5.54382C6.51676 6.26578 5.92779 7.08275 5.43381 7.99471C4.97783 8.86867 4.74984 9.89462 4.74984 11.0726C4.74984 12.1365 4.95883 12.8395 5.37681 13.1815C5.83279 13.5235 6.17478 13.6945 6.40277 13.6945C6.78275 13.6945 7.27673 13.5805 7.8847 13.3525C8.49267 13.1245 9.06265 13.0105 9.59462 13.0105C10.0886 13.0105 10.5826 13.1245 11.0766 13.3525C11.6085 13.5425 12.0835 13.8464 12.5015 14.2644C12.9195 14.6444 13.2615 15.1194 13.5274 15.6894C13.7934 16.2213 13.9264 16.8483 13.9264 17.5703Z" fill="#0A80BB"/>
+</svg>
+`;
+
+export const fiveStarIcon: string = `
+<svg width="143" height="22" viewBox="0 0 143 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.2502 0L15.0743 6.5199L22.5002 8.11678L17.4377 13.7432L18.2031 21.25L11.2502 18.2074L4.29736 21.25L5.06274 13.7432L0.000244141 8.11678L7.42616 6.5199L11.2502 0Z" fill="#FFCB45"/>
+<path d="M41.2502 0L45.0743 6.5199L52.5002 8.11678L47.4377 13.7432L48.2031 21.25L41.2502 18.2074L34.2974 21.25L35.0627 13.7432L30.0002 8.11678L37.4262 6.5199L41.2502 0Z" fill="#FFCB45"/>
+<path d="M71.2502 0L75.0743 6.5199L82.5002 8.11678L77.4377 13.7432L78.2031 21.25L71.2502 18.2074L64.2974 21.25L65.0627 13.7432L60.0002 8.11678L67.4262 6.5199L71.2502 0Z" fill="#FFCB45"/>
+<path d="M101.25 0L105.074 6.5199L112.5 8.11678L107.438 13.7432L108.203 21.25L101.25 18.2074L94.2974 21.25L95.0627 13.7432L90.0002 8.11678L97.4262 6.5199L101.25 0Z" fill="#FFCB45"/>
+<path d="M131.25 0L135.074 6.5199L142.5 8.11678L137.438 13.7432L138.203 21.25L131.25 18.2074L124.297 21.25L125.063 13.7432L120 8.11678L127.426 6.5199L131.25 0Z" fill="#FFCB45"/>
+</svg>
+`;
+
+export const quotes = [
+  {
+    quotation:
+      "I am so glad I made the decision to buy this awning! I wanted a SunSetter awning so I could still enjoy the outside in the rain with its protection. Also great to stay out of the sun!",
+    name: "Rita",
+    location: "Pennsylvania",
+    startIcon: fiveStarIcon,
+  },
+  {
+    quotation:
+      "Very well built product, works at the touch of a button. Very sturdy and great coverage. I would definitely recommend to a friend!",
+    name: "Jim",
+    location: "Pennsylvania",
+    startIcon: fiveStarIcon,
+  },
+  {
+    quotation:
+      "Our SunSetter Awning is in use 24/7. It's very easy to use, retracks when strong winds are encountered and protects our patio. Thank you SunSetter, we love our awning!",
+    name: "Bill",
+    location: "South Carolina",
+    startIcon: fiveStarIcon,
+  },
+  {
+    quotation:
+      "Having a SunSetter awning has enabled me to enjoy my deck which would have been way too hot if I didn't have it. I love my retractable awning!",
+    name: "Anne",
+    location: "North Carolina",
+    startIcon: fiveStarIcon,
+  },
+  {
+    quotation:
+      "Our patio is on a southern exposure and the low winter sun can be blinding through the sliding door mornings through mid-day. The SunSetter awning helps control the light and provides AC/Energy Savings. Works well for sunlight control over sliding glass door.",
+    name: "Ron",
+    location: "Colorado",
+    startIcon: fiveStarIcon,
+  },
+  {
+    quotation:
+      "I use my awning to keep sun off the living room door and windows. The awning goes up quickly and is so easy to operate. The design and construction are much better than I expected. If I needed another, I would not hesitate to buy another SunSetter.",
+    name: "Don",
+    location: "Texas",
+    startIcon: fiveStarIcon,
+  },
+];
