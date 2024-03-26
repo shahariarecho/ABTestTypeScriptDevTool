ba26cb5bf3a6ec9ec77fff75ed1e7208e582850e
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-03-26 23:03:39 +0600

    added scroll event

--- src/clients/brainlab/f5/F5014/common/test.info.ts
+++ src/clients/brainlab/f5/F5014/common/test.info.ts
@@ -1,1 +1,8 @@
-export enum TestInfo {ID = 'F5014', SITE = 'f5', TITLE = 'BLTEST044 - Footer Redirects for Glossary', CLIENT = 'brainlab', TARGET = 'https://www.f5.com/glossary/load-balancer', VARIATION = '1'}
+export enum TestInfo {
+  ID = "F5014",
+  SITE = "f5",
+  TITLE = "BLTEST044 - Footer Redirects for Glossary",
+  CLIENT = "brainlab",
+  TARGET = "https://www.f5.com/glossary/load-balancer",
+  VARIATION = "1",
+}
