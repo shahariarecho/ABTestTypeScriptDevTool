97810b52a4c667da024b29ce56d10cfc1937a844
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-04-01 23:39:05 +0600

    completed GB009

---
+++ src/clients/brainlab/graberblinds/GB009/common/studio.mode.ts
@@ -0,0 +1,10 @@
+export class StudioModel {
+  link: string = "";
+  round: number = 0;
+  title: string = "";
+  subTitle: string = "";
+  linkText: string = "";
+  location: string = "";
+  variation: string = "";
+  description: string = "";
+}
---
+++ src/clients/brainlab/graberblinds/GB009/components/studio.component.ts
@@ -0,0 +1,60 @@
+import { imgLink, selectors, triggerEvent } from "../common/asset";
+import { StudioModel } from "../common/studio.mode";
+
+export class StudioComponent {
+  private getHtml = (studio: StudioModel): string => {
+    const htmlString: string = `
+      <div class="studio-component round-${studio.round}" >
+        <div class="component-wrap" >
+          <div class="text-content" >
+            <div class="header" >
+              <div class="sub-title" >
+                <p>${studio.subTitle} ${studio.location}</p>
+              </div>
+              <div class="title" >
+                <h2>${studio.title}</h2>
+              </div>
+            </div>
+            <div class="description" >
+              <p>${studio.description}</p>
+            </div>
+            <div class="studio-link" >
+              <a href="${studio.link}" >${studio.linkText}</a>
+            </div>
+          </div>
+          <div class="image-content" > 
+            <img src="${imgLink}" alt="${studio.location}" >
+          </div>
+        </div>
+      </div>
+    `;
+    return htmlString.trim();
+  };
+
+  render = (studio: StudioModel) => {
+    const carousal: null | HTMLDivElement = document.querySelector(
+      selectors.carousal
+    );
+
+    if (!carousal) {
+      return;
+    }
+
+    carousal.insertAdjacentHTML("afterend", this.getHtml(studio));
+
+    this.addListener();
+  };
+
+  addListener = () => {
+    const link: null | HTMLAnchorElement =
+      document.querySelector("div.studio-link>a");
+
+    if (!link) {
+      return;
+    }
+
+    link.addEventListener("click", () => {
+      triggerEvent("studio-link-click");
+    });
+  };
+}
