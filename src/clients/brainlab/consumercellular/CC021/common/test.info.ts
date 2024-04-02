65b5584201452e19b423d02bc454d7ac1fa9dddc
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-04-02 22:15:28 +0600

    completed CC022

--- src/clients/brainlab/consumercellular/CC021/common/test.info.ts
+++ src/clients/brainlab/consumercellular/CC021/common/test.info.ts
@@ -1,1 +1,8 @@
-export enum TestInfo {ID = 'CC021', SITE = 'consumercellular', TITLE = '8.13.2 Messaging on Cart Summary', CLIENT = 'brainlab', TARGET = 'https://www.consumercellular.com/shopping', VARIATION = '3'}
+export enum TestInfo {
+  ID = "CC021",
+  SITE = "consumercellular",
+  TITLE = "8.13.2 Messaging on Cart Summary",
+  CLIENT = "brainlab",
+  TARGET = "https://www.consumercellular.com/shopping",
+  VARIATION = "3",
+}
---
+++ src/clients/brainlab/consumercellular/CC022/common/test.info.ts
@@ -0,0 +1,1 @@
+export enum TestInfo {ID = 'CC022', SITE = 'consumercellular', TITLE = '7.13 - Product selection colorway', CLIENT = 'brainlab', TARGET = 'https://www.consumercellular.com/shopping/details/moto_g_5g_2023/details', VARIATION = '1'}
