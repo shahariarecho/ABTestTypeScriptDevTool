143c7dc7196c6008f4bbb6924a132be54ec9b64d
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-04-30 01:21:44 +0600

    svg icon changed

--- src/clients/brainlab/f5/F5018/common/test.info.ts
+++ src/clients/brainlab/f5/F5018/common/test.info.ts
@@ -1,8 +1,1 @@
-export enum TestInfo {
-  ID = "F5018",
-  SITE = "f5",
-  TITLE = 'Add "By Cloud Partners" to Home Page',
-  CLIENT = "brainlab",
-  TARGET = "https://www.f5.com/",
-  VARIATION = "1",
-}
+export enum TestInfo {ID = 'F5018', SITE = 'f5', TITLE = 'Add "By Cloud Partners" to Home Page', CLIENT = 'brainlab', TARGET = 'https://www.f5.com/', VARIATION = '1'}
