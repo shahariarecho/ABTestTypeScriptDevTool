76c5aa836c5c5b98ec5e9c473d221968a411669d
Shahariar Kabir <35477596+ShimantoKabir@users.noreply.github.com>
2024-05-17 00:05:33 +0600

    building test

--- src/clients/brainlab/wetransfer/WT004/common/test.info.ts
+++ src/clients/brainlab/wetransfer/WT004/common/test.info.ts
@@ -1,8 +1,1 @@
-export enum TestInfo {
-  ID = "WT004",
-  SITE = "wetransfer",
-  TITLE = "10.1 - Sticky Banner - Primary CTA (get started)",
-  CLIENT = "brainlab",
-  TARGET = "https://wetransfer.com/explore/move-it-like-a-pro",
-  VARIATION = "1",
-}
+export enum TestInfo {ID = 'WT004', SITE = 'wetransfer', TITLE = '10.1 - Sticky Banner - Primary CTA (get started)', CLIENT = 'brainlab', TARGET = 'https://wetransfer.com/explore/move-it-like-a-pro', VARIATION = '1'}
